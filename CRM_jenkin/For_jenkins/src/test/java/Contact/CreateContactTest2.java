eab5b963e7449bb262161d88ee671634a5070633
ACER <ACER@DESKTOP-VHBPG3D>
2024-10-19 09:52:04 +0530

    New create contact commit

---
+++ CRM_jenkin/For_jenkins/src/test/java/Contact/CreateContactTest2.java
@@ -0,0 +1,11 @@
+package Contact;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactTest2 {
+	@Test
+	public void createContact2() {
+		Reporter.log("Created Contact Test 2", true);
+		}
+}
